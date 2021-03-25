36eb05d4aa2cda730d919302f10aac8edd700e65
jp <jpksh90@gmail.com>
2021-03-25 01:33:14 +0000

    changes

---
+++ projects/scripts/run.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+CFLAGS_INSTALL=$CFLAGS
+CXXFLAGS_INSTALL=$CXXFLAGS
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd ../$SUBJECT/ > /dev/null
+  ./project_config.sh
+popd > /dev/null
+
+#original f1x execution
+rm -rf original.txt
+pushd ../$SUBJECT/ > /dev/null
+  /src/f1x-oss-fuzz/repair/CInterface/profile -f $BUGGY_FILE -t $TESTCASE -T 6600 -d $DRIVER -b ./project_build.sh --output-one-per-loc -a -P /out -N $BINARY -M 16 &> $SCRIPT_DIR/original.txt
+  make clean
+  make distclean
+popd > /dev/null
+
+location=`cat ../$SUBJECT/location.txt`
+./afl-generateDistance.sh $location
+
+#export CFLAGS="$CFLAGS_INSTALL -distance=$OUT/distance.cfg.txt"
+#export CXXFLAGS="$CXXFLAGS_INSTALL -distance=$OUT/distance.cfg.txt"
+
+#execute f1x with fuzzing
+pushd ../$SUBJECT/ > /dev/null
+  make clean
+  make distclean
+  ./project_config.sh
+popd > /dev/null
+rm -rf f1x_with_fuzzing.txt
+
+./executeAFLGO
